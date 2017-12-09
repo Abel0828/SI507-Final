2607fec7b952f398ea987b8c267e902a0db5c081
Abel0828 <ywangdr@connect.ust.hk>
2017-12-08 21:29:54 -0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-db_name = 'postgres'
-db_user = 'postgres'
-db_password = '5237152371'
-
-plotly_api_key='TYp0zm6eNH3KHKwUWOWl'
-
-mapbox_access_token = 'pk.eyJ1IjoieXdhbmdkciIsImEiOiJjamF2YTBpeXU1bTY2MnFyemNxZ3g2enB2In0.OFHCn5mzcxq9zhoHQijTMA'
-
-geo_api_key='AIzaSyBaUUEcqYJIhVPWVZ-qOIu14JWsr5woqVE'
